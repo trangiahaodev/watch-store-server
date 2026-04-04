5f17bbbb85fb08faece520217c3dd0639ac2844d
allentran2606 <giahaot2606@gmail.com>
2026-04-04 11:42:24 +0700

    fix stats routes

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import { getAdminStats } from "../controllers/statsController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
+const router = express.Router();
+
 router.route("/admin-summary").get(protect, admin, getAdminStats);
 
 export default router;
