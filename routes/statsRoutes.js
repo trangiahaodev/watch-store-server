a0bcdcb4b059fa88baa922af756a63fef8cba122
allentran2606 <giahaot2606@gmail.com>
2026-04-04 11:44:41 +0700

    fix controller name

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAdminStats } from "../controllers/statsController.js";
+import { getDashboardStats } from "../controllers/statsController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/admin-summary").get(protect, admin, getAdminStats);
+router.route("/admin-summary").get(protect, admin, getDashboardStats);
 
 export default router;
