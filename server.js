d661ddc1e89b51c8b4f56c3b3b7b75f5518b27df
allentran2606 <giahaot2606@gmail.com>
2026-04-12 16:54:49 +0700

    feature: da xong api doc

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import statsRoutes from "./routes/statsRoutes.js";
-import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
 
 // 1. Config
 dotenv.config();
@@ -17,6 +21,13 @@ const app = express();
 // 2. Middleware
 app.use(cors()); // Cho phép Frontend gọi API
 app.use(express.json()); // Để đọc được JSON từ body request
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerPath = path.resolve(process.cwd(), "swagger.yaml");
+const swaggerDocument = yaml.load(swaggerPath);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // 3. Routes
 app.use("/api/products", productRoutes);
